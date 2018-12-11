b60783ef27ffe75084da4865e49d2f37b1b4e681
srinivasrk <srinivasrk92@gmail.com>
2018-12-11 14:39:24 -0500

    custom hpa test

---
+++ hpa/DockerImage/worker.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import pika
+import os
+import time
+import sys
+import random
+
+def recur_fibo(n):
+   """Recursive function to
+   print Fibonacci sequence"""
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+
+def receive_message():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq-service", 5672, '/', credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=os.environ['QUEUE'], durable=True)
+    print(" Worker Spwaning")
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        i = random.randint(100, 300) # generate a random number b/w 1000 and 9999
+        for k in range(i):
+            print(recur_fibo(k)) # find its Fibonacci
+        time.sleep(2)
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        sys.exit()
+
+    # send ack
+    channel.basic_consume(callback,
+                          queue=os.environ['QUEUE'])
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+receive_message()
