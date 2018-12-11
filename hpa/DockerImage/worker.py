e3a8cc1bde3a347d6da65a95ae1582a30190547f
srinivasrk <srinivasrk92@gmail.com>
2018-12-11 16:18:49 -0500

    use logger to get better insight

--- hpa/DockerImage/worker.py
+++ hpa/DockerImage/worker.py
@@ -7,6 +7,15 @@ import random
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
+import logging
+logger = logging.getLogger('hpa-custom')
+hdlr = logging.FileHandler('/hpa-custom.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.WARNING)
+
+
 def recur_fibo(n):
    """Recursive function to
    print Fibonacci sequence"""
@@ -23,12 +32,15 @@ def receive_message():
 
     channel.queue_declare(queue='hpa-custom', durable=True)
     print("Worker Spwaning")
+    logger.info('Worker Spwaning')
+
     def callback(ch, method, properties, body):
+        logger.info('Reading message')
         print(" [x] Received %r" % body)
-        i = random.randint(100, 300) # generate a random number b/w 1000 and 9999
-        for k in range(i):
-            print(recur_fibo(k)) # find its Fibonacci
-        time.sleep(2)
+        i = random.randint(100, 300)  # generate a random number b/w 1000 and 9999
+        # for k in range(i):
+        #     print(recur_fibo(k))  # find its Fibonacci
+        # time.sleep(2)
         print(" [x] Done")
         ch.basic_ack(delivery_tag=method.delivery_tag)
         return "done"
@@ -46,6 +58,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     try:
+        logger.info('received get request')
         receive_message()
         return "Done"
     except:
