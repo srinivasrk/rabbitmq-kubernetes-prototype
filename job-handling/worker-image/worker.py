090f784dac0dff04942c8401249ff4d9da7b3c7e
srinivasrk <srinivasrk92@gmail.com>
2018-12-11 12:43:58 -0500

    final changes

--- job-handling/worker-image/worker.py
+++ job-handling/worker-image/worker.py
@@ -1,28 +1,23 @@
-#!/usr/bin/env python
-
-# Just prints standard out and sleeps for 10 seconds.
-import sys
-import time
-print("Processing " + sys.stdin.lines())
-time.sleep(10)
-
+#!/usr/bin/env python3
 
 import pika
 import os
 import time
+import sys
 
 def receive_message():
-    credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
-    connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['BROKER_URL'], 5672, '/', credentials))
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq-service", 5672, '/', credentials))
     channel = connection.channel()
 
-    channel.queue_declare(queue=os.environ['QUEUE'])
-
+    channel.queue_declare(queue=os.environ['QUEUE'], durable=True)
+    print(" Worker Spwaning")
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
         time.sleep(2)
         print(" [x] Done")
         ch.basic_ack(delivery_tag=method.delivery_tag)
+        sys.exit()
 
     # send ack
     channel.basic_consume(callback,
